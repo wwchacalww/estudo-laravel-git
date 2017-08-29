18cc2c80af812743accfe24e13e30d1f43e3ebcf
wwChacalww <juniorcartaxo@gmail.com>
2017-08-29 09:16:01 -0300

    Implementação do segundo Bimestre nos Rendimentos

--- resources/views/rendimentos/index.blade.php
+++ resources/views/rendimentos/index.blade.php
@@ -68,16 +68,83 @@
               @if(count($turma->alunos->first()->rendimentos) > 0)
                 @foreach($turma->alunos->first()->rendimentos as $rendimento)
                   <?php
-                  if($rendimento->disciplina->habilidade == "Artes"){ $art = 'fa-check-square-o'; }
-                  if($rendimento->disciplina->habilidade == "Ciências Naturais"){ $cn = 'fa-check-square-o'; }
-                  if($rendimento->disciplina->habilidade == "Educação Física"){ $edf = 'fa-check-square-o'; }
-                  if($rendimento->disciplina->habilidade == "Geografia"){ $geo = 'fa-check-square-o'; }
-                  if($rendimento->disciplina->habilidade == "História"){ $hist = 'fa-check-square-o'; }
-                  if($rendimento->disciplina->habilidade == "Inglês"){ $lem = 'fa-check-square-o'; }
-                  if($rendimento->disciplina->habilidade == "Matemática"){ $mat = 'fa-check-square-o'; }
-                  if($rendimento->disciplina->habilidade == "Português"){ $por = 'fa-check-square-o'; }
-                  if($rendimento->disciplina->habilidade == "Práticas Diversificadas I"){ $pdi = 'fa-check-square-o'; }
-                  if($rendimento->disciplina->habilidade == "Práticas Diversificadas II"){ $pdii = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Artes" && $rendimento->bimestre == 1){ $art = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Ciências Naturais" && $rendimento->bimestre == 1){ $cn = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Educação Física" && $rendimento->bimestre == 1){ $edf = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Geografia" && $rendimento->bimestre == 1){ $geo = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "História" && $rendimento->bimestre == 1){ $hist = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Inglês" && $rendimento->bimestre == 1){ $lem = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Matemática" && $rendimento->bimestre == 1){ $mat = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Português" && $rendimento->bimestre == 1){ $por = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Práticas Diversificadas I" && $rendimento->bimestre == 1){ $pdi = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Práticas Diversificadas II" && $rendimento->bimestre == 1){ $pdii = 'fa-check-square-o'; }
+                  ?>
+                @endforeach
+              @endif
+              <tr>
+                <td>{{$turma->turma}}</td>
+                <td><i class="fa {{$art}}"></i></td>
+                <td><i class="fa {{$cn}}"></i></td>
+                <td><i class="fa {{$edf}}"></i></td>
+                <td><i class="fa {{$geo}}"></i></td>
+                <td><i class="fa {{$hist}}"></i></td>
+                <td><i class="fa {{$lem}}"></i></td>
+                <td><i class="fa {{$mat}}"></i></td>
+                <td><i class="fa {{$por}}"></i></td>
+                <td><i class="fa {{$pdi}}"></i></td>
+                <td><i class="fa {{$pdii}}"></i></td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+
+        <hr>
+
+        <h4>2º Bimestre - Controle de Lançamento de Notas</h4>
+        <hr>
+        <table class="table table-responsive">
+          <thead>
+            <tr>
+              <th>Turma</th>
+              <th>ART</th>
+              <th>CN</th>
+              <th>EDF</th>
+              <th>GEO</th>
+              <th>HIST</th>
+              <th>LEM</th>
+              <th>MAT</th>
+              <th>POR</th>
+              <th>PDI</th>
+              <th>PDII</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($turmas as $turma)
+              <?php
+                $art = 'fa-square-o';
+                $cn = 'fa-square-o';
+                $edf = 'fa-square-o';
+                $geo = 'fa-square-o';
+                $hist = 'fa-square-o';
+                $lem = 'fa-square-o';
+                $mat = 'fa-square-o';
+                $por = 'fa-square-o';
+                $pdi = 'fa-square-o';
+                $pdii = 'fa-square-o';
+              ?>
+              @if(count($turma->alunos->first()->rendimentos) > 0)
+                @foreach($turma->alunos->first()->rendimentos as $rendimento)
+                  <?php
+                  if($rendimento->disciplina->habilidade == "Artes" && $rendimento->bimestre == 2){ $art = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Ciências Naturais" && $rendimento->bimestre == 2){ $cn = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Educação Física" && $rendimento->bimestre == 2){ $edf = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Geografia" && $rendimento->bimestre == 2){ $geo = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "História" && $rendimento->bimestre == 2){ $hist = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Inglês" && $rendimento->bimestre == 2){ $lem = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Matemática" && $rendimento->bimestre == 2){ $mat = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Português" && $rendimento->bimestre == 2){ $por = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Práticas Diversificadas I" && $rendimento->bimestre == 2){ $pdi = 'fa-check-square-o'; }
+                  if($rendimento->disciplina->habilidade == "Práticas Diversificadas II" && $rendimento->bimestre == 2){ $pdii = 'fa-check-square-o'; }
                   ?>
                 @endforeach
               @endif
